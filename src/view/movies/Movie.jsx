1d3f36e07d5220f29a48b8ad3ba90d55771bd3b8
Karan Kapoor <kk8658141@gmail.com>
2022-08-24 18:12:09 +0530

    use useNavigate

--- src/view/movies/Movie.jsx
+++ src/view/movies/Movie.jsx
@@ -1,12 +1,14 @@
 import React, { useEffect, useState } from "react";
 import Footer from "../../Components/Footer/Footer";
 import Navbar from "../../Components/Navbar/Navbar";
+import { useNavigate } from "react-router-dom";
 import axios from "axios";
 import "./movie.css";
 // import Movies from "../HomePage/HomeMovies/Movies";
 
 export default function Movie() {
   const [movies, setMovies] = useState([]);
+  const navigate =  useNavigate();
 
   useEffect(() => {
     axios.defaults.headers = {
@@ -38,7 +40,8 @@ export default function Movie() {
                     <div className="movieImg">
                     <a onClick={() =>
                     {
-                      window.location.href = "/MovieInfo/" + data._id;
+                      // window.location.href = "/MovieInfo/" + data._id;
+                      navigate("/MovieInfo/" + data._id)
                     }}>
                         <img src={data.movieImage} alt="" />
                     </a>
